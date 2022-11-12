b0a2b848d48cc624fe762b4bfea49b6a1711a493
ErickFPCruz <erick.cruz97@hotmail.com>
2022-11-11 21:29:31 -0300

    Versão JS do Nome Completo feita!

---
+++ index.js
@@ -0,0 +1,5 @@
+let nome = prompt("Escreva seu primeiro nome: ");
+let sobrenome = prompt("Escreva seu sobrenome: ");
+
+alert(`Nome Completo: ${nome} ${sobrenome}`);
+alert(`Nome de Catálogo: ${sobrenome.toUpperCase()}, ${nome}`);
